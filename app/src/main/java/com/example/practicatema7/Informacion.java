f06e3d286850557b4740ad28076efe5715d1b8cf
MarioBurgos <mburgomaes@gmail.com>
2019-03-13 16:43:19 +0100

    Listo para entregar.

--- app/src/main/java/com/example/practicatema7/Informacion.java
+++ app/src/main/java/com/example/practicatema7/Informacion.java
@@ -1,7 +1,20 @@
 package com.example.practicatema7;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.practicatema7.DataBaseManager.DB_SQLite;
+import com.example.practicatema7.DataBaseManager.Esquema;
 
 public class Informacion extends AppCompatActivity {
 
@@ -9,5 +22,52 @@ public class Informacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+
+        TextView txt1       = findViewById(R.id.txt1);
+        TextView txt2       = findViewById(R.id.txt2);
+        TextView txt3       = findViewById(R.id.txt3);
+        TextView txt4       = findViewById(R.id.txt4);
+        RatingBar ratingBar = findViewById(R.id.ratingB);
+
+        //Recogemos los datos almacenados
+        txt1.setText(App.sitiosActivo.getNombre());
+        txt2.setText(App.sitiosActivo.getLatitud().toString());
+        txt3.setText(App.sitiosActivo.getLongitud().toString());
+        txt4.setText(App.sitiosActivo.getComentarios());
+        ratingBar.setRating(App.sitiosActivo.getValoracion());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.informenu, menu);
+        return true;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcionEditar:
+                App.SALIDA = 1;
+                startActivity(new Intent(getApplicationContext(), PantallaNuevoEdicion.class));
+                finish();
+                Toast.makeText(this, getResources().getString(R.string.Sitioedit), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.opcionBorrar:
+                eliminar();
+                finish();
+                break;
+        }
+        return false;
+    }
+
+    private void eliminar()
+    {
+        DB_SQLite db = new DB_SQLite(this);
+        SQLiteDatabase conn = db.getWritableDatabase();
+        String sqlWhere = Esquema.Sitios.COLUMN_NAME_ID + " LIKE '" + App.sitiosActivo.getId() + "'";
+        conn.delete(Esquema.Sitios.TABLE_NAME, sqlWhere, null);
+        Toast.makeText(this, getResources().getString(R.string.Sitioeliminado), Toast.LENGTH_LONG).show();
+        conn.close();
     }
 }
--- app/src/main/java/com/example/practicatema7/PantallaMapa.java
+++ app/src/main/java/com/example/practicatema7/PantallaMapa.java
@@ -1,13 +1,76 @@
 package com.example.practicatema7;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class PantallaMapa extends AppCompatActivity {
+import com.example.practicatema7.Logic.LogicSitios;
+import com.example.practicatema7.Model.Sitios;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class PantallaMapa extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LatLng Marcador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_mapa);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void mostrarMarcadores() {
+        List<Sitios> listaLugares = LogicSitios.listaSitios(this);
+        if (listaLugares == null) {
+        } else {
+
+            for (Sitios l : listaLugares) {
+                Marcador = new LatLng(l.getLatitud(), l.getLongitud());
+                if (l.getCategoria() == 1) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                } else if (l.getCategoria() == 2) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                } else if (l.getCategoria() == 3) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                } else if (l.getCategoria() == 4) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+                } else if (l.getCategoria() == 5) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                } else if (l.getCategoria() == 6) {
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+                }
+            }
+        }
     }
 }
